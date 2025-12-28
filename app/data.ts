a4bec4cb76874d4bdaf580d9e45cddc514a3500c
Ryan Loh <ryanloh28@yahoo.com.sg>
2025-12-29 04:24:10 +0800

    first commit

---
+++ app/data.ts
@@ -0,0 +1,95 @@
+type Project = {
+  name: string
+  description: string
+  link: string
+  image_url: string
+  id: string
+}
+
+type WorkExperience = {
+  company: string
+  title: string
+  start: string
+  link: string
+  id: string
+}
+
+type BlogPost = {
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
+type SocialLink = {
+  label: string
+  link: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Agentic Configuration Remediation',
+    description:
+      'Building agentic workflows to automate configuration monitoring and remediation',
+    link: 'https://github.com/Config-Guardians',
+    image_url: '/project-1.png',
+    id: 'project1',
+  },
+]
+
+export const WORK_EXPERIENCE: WorkExperience[] = [
+  {
+    company: 'Apple Inc.',
+    title: 'Site Reliability Engineer',
+    start: '2026',
+    link: 'https://apple.com',
+    id: 'work1',
+  },
+  {
+    company: 'Netvirta Inc.',
+    title: 'DevOps Engineer',
+    start: '2025',
+    link: 'https://netvirta.com',
+    id: 'work2',
+  },
+  {
+    company: 'Ensign Infosecurity',
+    title: 'DevOps Engineer',
+    start: '2024',
+    link: 'https://ensigninfosecurity.com/',
+    id: 'work3',
+  },
+  {
+    company: 'SMU Whitehats Society',
+    title: 'Technology Director',
+    start: '2023',
+    link: 'https://whitehats.computing.smu.edu.sg',
+    id: 'work4',
+  },
+]
+
+export const BLOG_POSTS: BlogPost[] = [
+  {
+    title: 'Building a homelab as a broke college student',
+    description: 'A deep dive into the why and how of getting into homelabbing',
+    link: '/blog/building-a-homelab',
+    uid: 'blog-1',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    label: 'Github',
+    link: 'https://github.com/mrchoc',
+  },
+  {
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/ryan-loh-3400521b3',
+  },
+  {
+    label: 'Resume',
+    link: '/resume.pdf',
+  },
+]
+
+export const EMAIL = 'ryanloh28@yahoo.com.sg'
---
+++ mdx-components.tsx
@@ -0,0 +1,31 @@
+import type { MDXComponents } from 'mdx/types'
+import { ComponentPropsWithoutRef } from 'react'
+import { highlight } from 'sugar-high'
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    ...components,
+    Cover: ({
+      src,
+      alt,
+      caption,
+      width
+    }: {
+      src: string
+      alt: string
+      caption: string
+      width: number
+    }) => {
+      return (
+        <figure>
+          <img src={src} alt={alt} className="rounded-xl mx-auto" width={width}/>
+          <figcaption className="text-center">{caption}</figcaption>
+        </figure>
+      )
+    },
+    code: ({ children, ...props }: ComponentPropsWithoutRef<'code'>) => {
+      const codeHTML = highlight(children as string)
+      return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+    },
+  }
+}
